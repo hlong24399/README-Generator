638160a5d9610d5abbe05314ce352a14adc7503d
Hoang Long <hlong24399@gmail.com>
2022-07-13 22:53:03 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,76 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is your project description?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What is the Installation?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the Usage?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What is the License?",
+    choices: ["MIT", "ISC", "IBM"],
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "What is the contribution?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What is the test?",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is the Username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the Email?",
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log("file read");
+    }
+  })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answer) => {
+    writeToFile("readme.md", generateMarkdown(answer));
+  });
+}
+
+// Function call to initialize app
+init();
---
+++ readme.md
@@ -0,0 +1,29 @@
+# BeautiFind
+  ## TABLE OF CONTENT
+  
+  [Description](#DESCRIPTION)  
+  [Installation](#INSTALLATION)  
+  [Contribution](#CONTRIBUTION)  
+  [Usage](#USAGE)  
+  [Test](#TEST)  
+  [Questions](#QUESTIONS)  
+  [License](#LICENSE)  
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  ## DESCRIPTION
+  A Website that enables users to connect with local technicians to find the best fit
+  ## INSTALLATION
+  Install the website
+  ## USAGE
+  use to connect people
+  ## CONTRIBUTION
+  Me
+  ## TEST
+  No Test Needed
+  ## QUESTIONS
+
+  This is my Github Profile : [Profile](https://github.com/hlong24399)  
+  For any related questions, feel free to contact me at hlong24399@gmail.com
+
+  ## LICENSE
+  Covered by MIT
